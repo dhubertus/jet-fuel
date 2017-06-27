c559a65df51fe175acfdbde93a532c9be85745e5
Dave Hubertus <davehubertus@gmail.com>
2017-06-26 20:28:52 -0600

    cards prepending on page load

--- src/card-holder.js
+++ src/card-holder.js
@@ -1,7 +1,19 @@
-const cardHolder = $('#card-holder')
 
-let fakeObj = [
+let fakeArray = [
   {title: 'yo', url: 'www.sup.com'},
   {title: 'hey', url: 'www.asfga.com'},
   {title: 'bro', url: 'www.dude.com'}
 ]
+
+$('document').ready(() => {
+  const cardHolder = $('#card-holder')
+
+  fakeArray.forEach((obj, i) => {
+    cardHolder.prepend(
+      `<article class='single-card'>
+        <h4>${obj.title}</h4>
+        <a href='http://www.google.com' target='_blank'>${obj.url}</a>
+      </article>`
+    )
+  })
+})
