d0d9a8584c0a8139f1a1022da479cc4dcec56f56
Dave Hubertus <davehubertus@gmail.com>
2017-06-27 14:06:50 -0600

    cards prepending

--- src/card-holder.js
+++ src/card-holder.js
@@ -1,19 +1,31 @@
-
+//NOTE: will be fetch
 let fakeArray = [
   {title: 'yo', url: 'www.sup.com'},
   {title: 'hey', url: 'www.asfga.com'},
   {title: 'bro', url: 'www.dude.com'}
 ]
 
-$('document').ready(() => {
-  const cardHolder = $('#card-holder')
+const cardHolder = $('#card-holder')
 
+$('#thing').on('click', () => {
+  //NOTE: POST TO SERVER
+  const newObj = {title: 'yo', url: 'www.sup.com'}
+  fakeArray.push(newObj)
+  //NOTE: PLACE ON PAGE
+  prependNewCard(newObj.title, newObj.url)
+})
+
+$('document').ready(() => {
   fakeArray.forEach((obj, i) => {
-    cardHolder.prepend(
-      `<article class='single-card'>
-        <h4>${obj.title}</h4>
-        <a href='http://www.google.com' target='_blank'>${obj.url}</a>
-      </article>`
-    )
+    prependNewCard(obj.title, obj.url)
   })
 })
+
+const prependNewCard = (title, url) => {
+  cardHolder.prepend(
+    `<article id='${Date.now()} ${5}' class='single-card'>
+      <h4>${title}</h4>
+      <a href='${'http://www.google.com'}' target='_blank'>${url}</a>
+    </article>`
+  )
+}
