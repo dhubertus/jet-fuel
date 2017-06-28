cb773a66b567a8fc0daaa3cc59eed89dfc143d59
Dave Hubertus <davehubertus@gmail.com>
2017-06-28 14:52:51 -0600

    redirect working on copy paste

--- public/src/card-input.js
+++ public/src/card-input.js
@@ -21,29 +21,11 @@ $('.url-btn').on('click', () => {
   const parentId = $('#folder-title')
   const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let shortenedUrl = "";
-  let domain;
-  let bitly;
 
-  for( var i=0; i < 5; i++ ){
+  for( var i=0; i < 8; i++ ){
     shortenedUrl += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  if(url.slice(0,5)==="https"){
-    domain = url.slice(8,url.length)
-  }else if(url.slice(0,4)==="http"){
-    domain = url.slice(7,url.length)
-  }else{
-    domain = url
-  }
 
-const base_url = `https://api-ssl.bitly.com/v3/link/lookup?url=http%3A%2F%2F${domain}%2F&access_token=${ACCESS_TOKEN}`
-fetch(base_url)
-.then(item => item.json())
-.then(item => bitly = item.data.link_lookup[0].aggregate_link || "error")
-.then(() => {
-    if(bitly==="error"){
-      alert("excuse me sah! please place a full http://url address where needed! ")
-      return null
-    }
   fetch(`/api/v1/single-folder?folder=${parentId.html()}`)
   .then((res) => res.json())
   .then(obj => {
@@ -54,7 +36,7 @@ fetch(base_url)
         categories_id: obj[0].id,
         title:title,
         url:url,
-        url_shortened:bitly
+        url_shortened:`www.jetfuel/${shortenedUrl}.com`
         })
       }
     )
@@ -64,9 +46,8 @@ fetch(base_url)
       .then(list => list.json())
       .then(list => {
         console.log(list)
-        prependCard(list)
+        prependSingleCard(list)
       })
     })
   })
- })
 })
