eac8dfab8cd23e43ef401370fd238e8760c7c162
Dave Hubertus <davehubertus@gmail.com>
2017-06-27 21:19:27 -0600

    emptying children on folder change

--- public/src/card-input.js
+++ public/src/card-input.js
@@ -3,7 +3,21 @@ const month = d.getMonth()+1
 const day   = d.getDate()
 const year  = d.getFullYear()
 const ACCESS_TOKEN = 'ad6420e4b76dd1083ad965a0e4840952bb746972'
+
+const prependSingleCard = (array) => {
+
+  array.forEach((obj, i) => {
+    $('#card-holder').prepend(
+      `<article id='${Date.now()} ${5}' class='single-card'>
+        <h4>${obj.title}</h4>
+        <a href='${obj.url}' target='_blank'>${obj.url_shortened}</a>
+      </article>`
+    )
+  })
+}
+
 $('.url-btn').on('click', () => {
+
 const base_url = `https://api-ssl.bitly.com/v3/link/lookup?url=http%3A%2F%2Fcnn.com%2F&access_token=${ACCESS_TOKEN}`
 fetch(base_url)
 .then(item => item.json())
@@ -13,8 +27,7 @@ fetch(base_url)
 
 
 
-  return null
-  console.log("WOOO");
+  // return null
   const title = $('.title-input').val()
   const url = $('.url-input').val()
   const parentId = $('#folder-title')
@@ -42,12 +55,13 @@ fetch(base_url)
     )
     .then(obj => obj.json())
     .then(obj => {
-      console.log(obj);
       fetch(`/api/v1/folder-urls?id=${obj[0]}`)
       .then(list => list.json())
-      .then(list => console.log(list,"list!!!!"))
-
-  })
+      .then(list => {
+        console.log(list)
+        prependCard(list)
+      })
+    })
   })
   .catch(err => console.log(err))
 })
