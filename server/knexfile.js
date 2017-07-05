fbe290c7f2c066bf24601ac731c643d1c198ac02
Dave Hubertus <davehubertus@gmail.com>
2017-07-05 12:19:42 -0600

    added staging to knex file

--- server/knexfile.js
+++ server/knexfile.js
@@ -31,5 +31,16 @@ module.exports = {
       directory: 'db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: 'db/migrations'
+    },
+    seeds: {
+      directory: 'db/seeds/dev'
+    },
+    useNullAsDefault: true
   }
 };
