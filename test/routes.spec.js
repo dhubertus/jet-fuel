6814759073bf808261ace97b0ed3a7bd097c5906
Dave Hubertus <davehubertus@gmail.com>
2017-07-06 08:43:25 -0600

    dh-testing-attempt

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,7 +4,7 @@ const should = chai.should()
 const chaiHttp = require('chai-http')
 const server = require('../server/server.js')
 const knex = require('../server/db/knex.js')
-const config = require('../server/knexfile.js')
+// const config = require('../server/knexfile.js')
 
 chai.use(chaiHttp)
 
@@ -32,18 +32,46 @@ describe('Client Routes', () => {
 
 describe('API Routes', () => {
 
+  before((done) => {
+    // Run migrations and seeds for test database
+    // knex.migrate.latest()
+    knex.seed.run()
+    .then(() => {
+      done()
+    })
+  });
+
+  // before(done => {
+  //   knex.migrate.rollback()
+  //   .then(() => {
+  //     knex.migrate.latest()
+  //     .then(() => {
+  //       return knex.seed.run()
+  //       .then(() => {
+  //         done();
+  //       });
+  //     });
+  //   });
+  // });
+
+  // beforeEach((done) => {
+  //   knex.seed.run()
+  //   done()
+  // })
+
   it('should return all of the categories', (done) => {
     chai.request(server)
       .get('/api/v1/categories')
       .end((err, res) => {
-        // res.should.have.status(200)
-        // res.should.be.json;
-        // res.body.should.be.a('array');
-        // res.body.length.should.equal(3);
-        // res.body[0].should.have.property('folder');
+        // console.log(res.body, 'all cat');
+        res.should.have.status(200)
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.equal(3);
+        res.body[0].should.have.property('folder');
         // res.body[0].folder.should.equal('sports');
-        // res.body[0].should.have.property('id');
-        // res.body[0].id.should.equal(1);
+        res.body[0].should.have.property('id');
+        res.body[0].id.should.equal(1);
         done()
       })
     })
@@ -52,14 +80,14 @@ describe('API Routes', () => {
     chai.request(server)
       .get('/api/v1/single-folder?folder=sports')
       .end((err, res) => {
-        // res.should.have.status(200)
-        // res.should.be.json;
-        // res.body.should.be.a('array');
-        // res.body.length.should.equal(1);
-        // res.body[0].should.have.property('folder');
-        // res.body[0].folder.should.equal('sports');
-        // res.body[0].should.have.property('id');
-        // res.body[0].id.should.equal(1);
+        res.should.have.status(200)
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.equal(1);
+        res.body[0].should.have.property('folder');
+        res.body[0].folder.should.equal('sports');
+        res.body[0].should.have.property('id');
+        res.body[0].id.should.equal(1);
         done()
       })
     })
@@ -68,7 +96,7 @@ describe('API Routes', () => {
       chai.request(server)
         .get('/api/v1/single-folder?folder=sad')
         .end((err, res) => {
-          // res.should.have.status(404)
+          res.should.have.status(404)
           done()
         })
       })
@@ -77,24 +105,30 @@ describe('API Routes', () => {
       chai.request(server)
         .get('/api/v1/folder-urls?id=1')
         .end((err, res) => {
-          // res.should.have.status(200)
-          // res.should.be.json;
-          // res.body.should.be.a('array');
-          // res.body.length.should.equal(2);
-          // res.body[0].should.have.property('id');
-          // res.body[0].id.should.equal(1);
-          // res.body[0].should.have.property('title');
-          // res.body[0].title.should.equal('google');
-          // res.body[0].should.have.property('url');
-          // res.body[0].url.should.equal('https://google.com');
-          // res.body[0].should.have.property('visits');
-          // res.body[0].visits.should.equal(3);
-          // res.body[0].should.have.property('url_shortened');
-          // res.body[0].url_shortened.should.equal('googs');
-          // res.body[0].should.have.property('categories_id');
-          // res.body[0].categories_id.should.equal(1);
-          // res.body[0].should.have.property('created_at');
-          // res.body[0].should.have.property('updated_at');
+
+          const sorted = res.body.sort((a,b) => {
+            return a.id - b.id
+          })
+
+          // console.log(res.body, 'url-list');
+          res.should.have.status(200)
+          res.should.be.json;
+          sorted.should.be.a('array');
+          sorted.length.should.equal(2);
+          sorted[0].should.have.property('id');
+          sorted[0].id.should.equal(1);
+          sorted[0].should.have.property('title');
+          sorted[0].title.should.equal('google');
+          sorted[0].should.have.property('url');
+          sorted[0].url.should.equal('https://google.com');
+          sorted[0].should.have.property('visits');
+          sorted[0].visits.should.equal(3);
+          sorted[0].should.have.property('url_shortened');
+          sorted[0].url_shortened.should.equal('googs');
+          sorted[0].should.have.property('categories_id');
+          sorted[0].categories_id.should.equal(1);
+          sorted[0].should.have.property('created_at');
+          sorted[0].should.have.property('updated_at');
           done()
         })
       })
@@ -104,45 +138,31 @@ describe('POST Routes', () => {
 
   // before((done) => {
   //   // Run migrations and seeds for test database
-  //   done()
+  //   // knex.migrate.latest()
+  //   knex.seed.run()
+  //   .then(() => {
+  //     done()
+  //   })
   // });
 
   // beforeEach((done) => {
-  //   // Would normally run run your seed(s), which includes clearing all records
-  //   // from each of the tables
-  //   // server.locals.students = students;
-  //   knex.migrate.rollback()
-  //     .then(() => {
-  //       knex.migrate.latest()
-  //         .then(() => {
-  //           return knex.seed.run()
-  //             // .then(() => {
-  //             //   done();
-  //             // })
-  //         })
-  //     })
+  //   knex.seed.run()
   //   done()
-  // });
-  //
-  // afterEach((done) => {
-  //   knex.migrate.rollback()
-  //     // .then(() => {
-  //     //   done();
-  //     // });
-  //   done()
-  // });
+  // })
 
   it('should make a new folder' , (done) => {
     chai.request(server)
       .post('/api/v1/categories')
       .send({
-        folder: 'newGuy'
+        folder: 'newGuy',
+        id: 10
       })
       .end((err, res) => {
-        // res.should.have.status(200)
-        // res.should.be.json;
-        // res.body.should.be.a('array');
-        // res.body.should.deep.equal(['newGuy']);
+        // console.log(res.body, 'new folder');
+        res.should.have.status(200)
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.deep.equal(['newGuy']);
         done()
       })
     })
@@ -152,6 +172,7 @@ describe('POST Routes', () => {
     chai.request(server)
       .post('/api/v1/url')
       .send({
+        id: 500,
         title: 'blah',
         url:'bleep.com',
         visits:0,
@@ -159,35 +180,37 @@ describe('POST Routes', () => {
         categories_id:'2',
       })
       .end((err, res) => {
-        // res.should.have.status(200)
-        // res.should.be.json;
-        // res.body.should.be.a('array');
-        // res.body.length.should.equal(1)
-        // res.body[0].should.equal(2)
+        res.should.have.status(200)
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.equal(1)
+        res.body[0].should.equal(2)
         done()
       })
     })
 
-  // it('should update visits', (done) => {
-  //   chai.request(server)
-  //     .put('/api/v1/url/visit')
-  //     .send({
-  //         shortenedUrl:"googs"
-  //       })
-  //     .end((err,res) => {
-  //       // res.should.have.status(200)
-  //       // res.body.should.equal(1)
-  //       done()
-  //     })
-  //   chai.request(server)
-  //     .put('/api/v1/url/visit')
-  //     .send({
-  //         shortenedUrl:"googs"
-  //       })
-  //     .end((err,res) => {
-  //       // res.should.have.status(200)
-  //       // res.body.should.equal(2)
-  //       done()
-  //     })
-  //   })
+  it('should update visits', (done) => {
+    chai.request(server)
+      .put('/api/v1/url/visit')
+      .send({
+          shortenedUrl:"googs"
+        })
+      .end((err,res) => {
+        // console.log(res.body, 'visits 1');
+        res.should.have.status(200)
+        res.body.should.equal(1)
+        done()
+      })
+    chai.request(server)
+      .put('/api/v1/url/visit')
+      .send({
+          shortenedUrl:"googs"
+        })
+      .end((err,res) => {
+        // console.log(res.body, 'visits 2');
+        res.should.have.status(200)
+        res.body.should.equal(3)
+        done()
+      })
+    })
   })
